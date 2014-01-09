possan/sublime_unicode_nbsp

commit c7aa39c04724c1188d38a4d44f6c1b6b1833d1c5

Fix sublime_unicode_nbsp for sublime text 3

sublime_unicode_nbsp.py
@@ -15,7 +15,7 @@ def view_is_too_big(view, max_size_setting, default_max_size=None):
     settings = view.settings()
     max_size = settings.get(max_size_setting, default_max_size)
     if max_size not in (None, False):
-        max_size = long(max_size)
+        max_size = int(max_size)
         cur_size = view.size()
         if cur_size > max_size:
             return True
@@ -127,4 +127,4 @@ class HighlightUnicodeListener(DeferedViewListener):
         #for x in view.find_all(u'^$'):
         #    regions.append(x)
         view.add_regions('HighlightUnicodeJunk', regions, color_name,
-                         sublime.DRAW_EMPTY_AS_OVERWRITE)
+                         flags=sublime.DRAW_EMPTY_AS_OVERWRITE)

commit 58aa16d44008a64d4ac1644b319a1f59d8465b15

Fix sublime_unicode_nbsp for both sublime text 2&3
c7aa39c04 broke compatibility with sublime text 2. This one works on
both 2 and 3.

sublime_unicode_nbsp.py
@@ -126,5 +126,6 @@ class HighlightUnicodeListener(DeferedViewListener):
 
         #for x in view.find_all(u'^$'):
         #    regions.append(x)
-        view.add_regions('HighlightUnicodeJunk', regions, color_name,
-                         flags=sublime.DRAW_EMPTY_AS_OVERWRITE)
+        icon = ''
+        flags = sublime.DRAW_EMPTY_AS_OVERWRITE
+        view.add_regions('HighlightUnicodeJunk', regions, color_name, icon, flags)